b944f7dfa2f04afa8484e34c06224ec5cb16f594
d4goat <kenzoalgundul@gmail.com>
2026-01-22 14:46:35 +0700

    first commit

---
+++ app/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "~/components/ui/button";
+
+const Home = () => {
+    return (
+        <main className="min-h-screen">
+            <Button>ssss</Button>
+        </main>
+    )
+}
+
+export default Home
